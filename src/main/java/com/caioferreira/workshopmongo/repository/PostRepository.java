213fa78601be587745d8e3e398f46572e8aa867a
Caio Ferreira <136653258+CaioLira18@users.noreply.github.com>
2025-04-27 17:47:11 -0300

    [feat] - Creating Entity Post with Nested User

---
+++ src/main/java/com/caioferreira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.caioferreira.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.caioferreira.workshopmongo.domain.Post;
+import com.caioferreira.workshopmongo.domain.User;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+}
