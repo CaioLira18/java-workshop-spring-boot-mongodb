40d6a2b125e82429a9fe52009f552d1c3bbb6882
Caio Ferreira <136653258+CaioLira18@users.noreply.github.com>
2025-04-28 13:55:43 -0300

    [feat] - Multi Query

--- src/main/java/com/caioferreira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/caioferreira/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.caioferreira.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.caioferreira.workshopmongo.domain.Post;
@@ -11,6 +13,12 @@ import com.caioferreira.workshopmongo.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	// ?0 = Primeiro Parametro do Metodo
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	@Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2}  } , { $or: [ { 'title' : { $regex: ?0, $options: 'i' } }, { 'body' : { $regex: ?0, $options: 'i' } } , { 'comments.text' : { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
