88ab58a150db4367b7f81959b8eb5ea5d3b07122
Caio Ferreira <136653258+CaioLira18@users.noreply.github.com>
2025-04-28 12:22:43 -0300

    [feat] - Simple query with query methods

--- src/main/java/com/caioferreira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/caioferreira/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.caioferreira.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,6 @@ import com.caioferreira.workshopmongo.domain.User;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
--- src/main/java/com/caioferreira/workshopmongo/resources/PostResource.java
+++ src/main/java/com/caioferreira/workshopmongo/resources/PostResource.java
@@ -1,32 +1,40 @@
 package com.caioferreira.workshopmongo.resources;
 
 
-import java.util.Optional;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caioferreira.workshopmongo.domain.Post;
-import com.caioferreira.workshopmongo.repository.PostRepository;
+import com.caioferreira.workshopmongo.resources.util.URL;
+import com.caioferreira.workshopmongo.services.PostService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value="/posts")
 public class PostResource {
-	
-	@Autowired
-	private PostRepository service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Optional<Post>> findById(@PathVariable String id) throws ObjectNotFoundException {
-		Optional<Post> obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);		
-	}	
-	
-	
+    
+    @Autowired
+    private PostService service;  
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) throws ObjectNotFoundException {
+        Post obj = service.findById(id); 
+        return ResponseEntity.ok().body(obj);        
+    }    
+    
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue= "") String text) throws ObjectNotFoundException {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+    
+        return ResponseEntity.ok().body(list);        
+    }    
 }
+		
